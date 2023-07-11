36e513a02422103ec8fcc3a0175b875dc8d13c3c
Do Khac Duong <90135701+dokhacduong1@users.noreply.github.com>
2023-07-11 16:20:05 +0700

    first commit2

--- APITele.py
+++ APITele.py
@@ -79,27 +79,32 @@ import re
 from selenium.webdriver.chrome.options import Options
 import time
 
+
+
 dataShoppe = []
-store_data = open('db.json', 'w')
 chrome_options = Options()
 chrome_options.add_argument("--user-data-dir=C:/Users/Admin/AppData/Local/Google/Chrome/User Data/Profile 1")
-
+store_data = open('db.json', 'w')
 driver = webdriver.Chrome(options=chrome_options)
-urlOne = 'https://shopee.vn/search?keyword=s%C3%B4%20c%C3%B4%20la&page=0'
-driver.get(urlOne)
-for request in driver.requests:
-   if 'https://shopee.vn/api/v4/search/search_items?by=relevancy' in request.url:     
-      response = request.response
-      body = decode(response.body,response.headers.get('Content-Encoding','Identity'))
-      decode_body = body.decode('utf8')
-      json_data = json.loads(decode_body)
-      for x in json_data['items']:
-         x['item_basic'].pop('label_ids')
-         x['item_basic'].pop('tier_variations')
-         keys_to_removeItemTwo = [key for key, value in x['item_basic'].items() if value is None]
-         for key in keys_to_removeItemTwo:
-            x['item_basic'].pop(key)
-         dataShoppe.append(x['item_basic'])
+
+
+
+for i in range(9):
+   urlOne = 'https://shopee.vn/search?keyword=s%C3%B4%20c%C3%B4%20la&page='+str(i)
+   driver.get(urlOne)
+   for request in driver.requests:
+      if 'https://shopee.vn/api/v4/search/search_items?by=relevancy' in request.url:     
+         response = request.response
+         body = decode(response.body,response.headers.get('Content-Encoding','Identity'))
+         decode_body = body.decode('utf8')
+         json_data = json.loads(decode_body)
+         for x in json_data['items']:
+            x['item_basic'].pop('label_ids')
+            x['item_basic'].pop('tier_variations')
+            keys_to_removeItemTwo = [key for key, value in x['item_basic'].items() if value is None]
+            for key in keys_to_removeItemTwo:
+               x['item_basic'].pop(key)
+            dataShoppe.append(x['item_basic'])
 objNew = {
     "items": dataShoppe
 }
